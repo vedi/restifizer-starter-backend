cc07fceddacef9a31c03465b1bcda93cb1e5cb0f
vedi <shubin.fedor@gmail.com>
2020-04-30 10:15:56 +0300

    Support flag TS_NODE_TRANSPILE_ONLY

--- root-require.js
+++ root-require.js
@@ -1,5 +1,6 @@
 'use strict';
 
-require('ts-node').register({ transpileOnly: process.env.NODE_ENV === 'production' });
+const transpileOnly = process.env.NODE_ENV === 'production' || Boolean(process.env.TS_NODE_TRANSPILE_ONLY);
+require('ts-node').register({ transpileOnly });
 
 require('app-module-path').addPath(__dirname);
